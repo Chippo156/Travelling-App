ee0c44dd10e2d5d35a6547d043e8c1e5fedd4f23
NgoDat2003 <ngodat200319@gmail.com>
2024-11-01 20:52:06 +0700

    login

--- App.js
+++ App.js
@@ -6,13 +6,15 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { View, Text } from 'react-native';
 import Splash from './components/Splash';
 import Onboard from './components/Onboard';
+import Login from './components/Login';
 const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Onboard">
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Home" component={Splash} options={{ headerShown: false }} />
         <Stack.Screen name="Onboard" component={Onboard} options={{ headerShown: false }} />
+        <Stack.Screen name="Login" component={Login}  />
       </Stack.Navigator>
     </NavigationContainer>
   );
