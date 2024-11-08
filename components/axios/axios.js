d688593e27232707f6c3ae7ce679ce1bb6491c6d
Chippo156 <vovannghiahiep150623@gmail.com>
2024-11-08 22:36:57 +0700

    update details

--- components/axios/axios.js
+++ components/axios/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Khởi tạo instance axios
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api/v1", // URL mặc định cho tất cả các yêu cầu
+  baseURL: "https://travelling-app.onrender.com/api/v1", // URL mặc định cho tất cả các yêu cầu
   headers: {
     "Content-Type": "application/json",
   },
